2cfcfebd31633ec97cc6857b6134ecff30f9885b
Shynderio <nguyenducthinhch2@gmail.com>
2024-02-28 14:31:08 +0700

    reload program.cs

--- FinancialPlanning/FinancialPlanningAPI/Program.cs
+++ FinancialPlanning/FinancialPlanningAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+builder.Services.AddDbContext<FinancialPlanningDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
     b => b.MigrationsAssembly("FinancialPlanningAPI")));
 
 var app = builder.Build();
