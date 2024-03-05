5fbfc235f14d41bd3042c100ae48334ad2aeaec9
Shynderio <nguyenducthinhch2@gmail.com>
2024-03-05 16:54:57 +0700

    merge conflict

--- FinancialPlaningUI/FinancialPlaning/src/app/app.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/app.component.ts
@@ -4,11 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
->>>>>>> UC01-Login
 
 @Component({
   selector: 'app-root',
@@ -16,13 +12,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     RouterOutlet,
     LoginComponent,
-<<<<<<< HEAD
- 
-    CommonModule
-=======
     CommonModule,
     ReactiveFormsModule
->>>>>>> UC01-Login
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- FinancialPlaningUI/FinancialPlaning/src/app/pages/login/login.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/pages/login/login.component.ts
@@ -1,57 +1,25 @@
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-=======
 import { Component, model } from '@angular/core';
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
->>>>>>> UC01-Login
 import { LoginModel } from '../../models/loginModel.model';
 import { AuthService } from '../../services/auth.service';
 import jwt_decode from 'jwt-decode';
 import { HttpClient } from '@angular/common/http';
 import { response } from 'express';
-<<<<<<< HEAD
-=======
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 
 
 
->>>>>>> UC01-Login
 
 @Component({
   selector: 'app-login',
   standalone: true,
-<<<<<<< HEAD
-  imports: [FormsModule,CommonModule],
-=======
   imports: [FormsModule, CommonModule,ReactiveFormsModule],
->>>>>>> UC01-Login
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-<<<<<<< HEAD
-  
-  loginModel: LoginModel = {email: '',password: ''};
-  loggedInUser: any; // Khai báo biến để lưu thông tin người dùng đã đăng nhập
-
-  constructor(private authService: AuthService,private http: HttpClient) {}
-  login(): void {
-    this.http.post("https://localhost:7270/api/Auth/Login",this.loginModel).subscribe({
-      next: (response) => {
-        console.log(response); // Log response to the console
-    
-      },
-      error: (error) => {
-        console.error(error); // Log error to the console
-     
-      }
-    });
-  }
-  }
-=======
 
   loginModel: LoginModel = { email: '', password: '' };
   loggedInUser: any; // Khai báo biến để lưu thông tin người dùng đã đăng nhập
@@ -96,5 +64,4 @@ export class LoginComponent {
     });
   }
 }
->>>>>>> UC01-Login
 
