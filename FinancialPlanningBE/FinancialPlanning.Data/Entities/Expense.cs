60f50387d4a05d3f17c1c7d98d68135f8ff77826
Nguyen Tran Minh (K17 HL) <minhnthe176001@fpt.edu.vn>
2024-03-06 16:26:11 +0700

    Set some properties to nullable

--- FinancialPlanningBE/FinancialPlanning.Data/Entities/Expense.cs
+++ FinancialPlanningBE/FinancialPlanning.Data/Entities/Expense.cs
@@ -10,12 +10,11 @@ public class Expense
     public string CostType { get; set; } = string.Empty;
     public decimal UnitPrice { get; set; }
     public decimal Amount { get; set; }
-    public string Currency { get; set; } = string.Empty;
-    public double ExchangeRate { get; set; }
+    public string? Currency { get; set; }
+    public double? ExchangeRate { get; set; }
     public decimal TotalAmount { get; set; }
     public string ProjectName { get; set; } = string.Empty;
     public string SupplierName { get; set; } = string.Empty;
     public string PIC { get; set; } = string.Empty;
     public string? Note { get; set; } = string.Empty;
-    public Status Status { get; set; }
 }
