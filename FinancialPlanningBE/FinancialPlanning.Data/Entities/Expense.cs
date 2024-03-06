99ad67d11ab2f0de797f79104ed23b372d9c87d2
Nguyen Tran Minh (K17 HL) <minhnthe176001@fpt.edu.vn>
2024-03-06 12:52:12 +0700

    Remove unused files

---
+++ FinancialPlanningBE/FinancialPlanning.Data/Entities/Expense.cs
@@ -0,0 +1,21 @@
+namespace FinancialPlanning.Data.Entities;
+
+public class Expense
+{
+    public int No { get; set; }
+    public DateTime Date { get; set; }
+    public string Term { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public string ExpenseName { get; set; } = string.Empty;
+    public string CostType { get; set; } = string.Empty;
+    public decimal UnitPrice { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = string.Empty;
+    public double ExchangeRate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string ProjectName { get; set; } = string.Empty;
+    public string SupplierName { get; set; } = string.Empty;
+    public string PIC { get; set; } = string.Empty;
+    public string? Note { get; set; } = string.Empty;
+    public Status Status { get; set; }
+}
---
+++ FinancialPlanningBE/FinancialPlanning.WebAPI/Program.cs
@@ -0,0 +1,42 @@
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.EntityFrameworkCore;
+using FinancialPlanning.Data.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    b => b.MigrationsAssembly("FinancialPlanning.WebAPI")));
+
+builder.Services.AddAWSService<IAmazonS3>(new AWSOptions
+{
+    Credentials = new BasicAWSCredentials(builder.Configuration["AWS:AccessKey"], builder.Configuration["AWS:SecretKey"]),
+    Region = RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"])
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
