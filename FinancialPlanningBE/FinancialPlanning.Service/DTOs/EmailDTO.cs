e6237542bf0cfebc89cf98638c514fe491e5b358
Nguyen Tran Minh (K17 HL) <minhnthe176001@fpt.edu.vn>
2024-03-09 21:04:50 +0700

    Remove redundant files

--- FinancialPlanningBE/FinancialPlanning.Service/DTOs/EmailDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace FinancialPlanning.Service.DTOs
-{
-    public class EmailDTO
-    {
-        public string To { get; set; } = null!;
-        public string Subject { get; set; } = null!;
-        public string Body { get; set; } = null!;
-    }
-}
