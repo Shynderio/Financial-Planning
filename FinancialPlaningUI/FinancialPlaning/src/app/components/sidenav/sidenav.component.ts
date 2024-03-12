ee28e699bcb17012937b495e01a24f55df7eec55
VinhNQ45 <VinhNQ45.fpt.com>
2024-03-12 16:31:54 +0700

    Remove logout component and add it in sidenav

--- FinancialPlaningUI/FinancialPlaning/src/app/components/sidenav/sidenav.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/components/sidenav/sidenav.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterLink, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Renderer2, model } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginModel } from '../../models/loginModel.model';
+import { AuthService } from '../../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl,FormBuilder,AbstractControl } from '@angular/forms';
 import { HomeComponent } from '../../pages/home/home.component';
-import { LogoutComponent } from '../../pages/logout/logout.component';
+
+
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [HomeComponent, RouterLink,LogoutComponent],
+  imports: [HomeComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
 
+  constructor(
+    private authService: AuthService, 
+    private http: HttpClient,
+    private router: Router, 
+  ) { 
+  }
+  logout() {
+    if (confirm('Are you sure you want to logout?')) {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+    }
+  }
+  ngOnInit(): void {}
 }
