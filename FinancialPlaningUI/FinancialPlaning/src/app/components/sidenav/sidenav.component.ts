1803cd8ce9a78e8088f3d0e0dd75c2cfcc6e3614
VinhNQ45 <VinhNQ45.fpt.com>
2024-03-12 10:04:24 +0700

    do logout

--- FinancialPlaningUI/FinancialPlaning/src/app/components/sidenav/sidenav.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, Routes } from '@angular/router';
 import { HomeComponent } from '../../pages/home/home.component';
+import { LogoutComponent } from '../../pages/logout/logout.component';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [HomeComponent, RouterLink],
+  imports: [HomeComponent, RouterLink,LogoutComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
