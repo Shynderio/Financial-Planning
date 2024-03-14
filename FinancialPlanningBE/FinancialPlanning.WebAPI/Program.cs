10e3ba8cbf5e397edeba5e814b18b49b0f29fcfa
DESKTOP-8D3LMBI\hihih <hihihi652652@gmail.com>
2024-03-14 12:23:43 +0700

    configration

--- FinancialPlanningBE/FinancialPlanning.WebAPI/Program.cs
+++ FinancialPlanningBE/FinancialPlanning.WebAPI/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection"),
-    b => b.MigrationsAssembly("FinancialPlanning.Data")));
+    b => b.MigrationsAssembly("FinancialPlanning.WebAPI")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
