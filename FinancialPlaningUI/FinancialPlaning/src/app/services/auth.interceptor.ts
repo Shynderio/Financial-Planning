35b4269780a4c8d7c1e40f91938fd8b9e98a9a02
VinhNQ45 <VinhNQ45.fpt.com>
2024-03-15 09:20:40 +0700

    add when login can click enter

--- FinancialPlaningUI/FinancialPlaning/src/app/services/auth.interceptor.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/services/auth.interceptor.ts
@@ -1,6 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { error } from 'console';
+import { CookieService } from 'ngx-cookie-service';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (request, next) => {
+  
   if (typeof localStorage !== 'undefined') {
     const token = localStorage.getItem('token') ?? '';
     request = request.clone({
@@ -10,6 +15,12 @@ export const authInterceptor: HttpInterceptorFn = (request, next) => {
     });
   }
   console.log("my message: ", request);
-
-  return next(request);
+  return next(request)
+  .pipe(catchError((error) =>{
+    if([401, 403].includes(error.statusCode)){
+      console.log('Unauthrized request');
+      window.location.href = '/login';
+    }
+    return throwError(() => error);
+  }));
 };
