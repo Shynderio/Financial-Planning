532b3e468e22f895546cf2bc7b952524503f223b
DESKTOP-8D3LMBI\hihih <hihihi652652@gmail.com>
2024-03-18 06:30:00 +0700

    Update PlanController

--- FinancialPlanningBE/FinancialPlanning.WebAPI/Controllers/PlanController.cs
+++ FinancialPlanningBE/FinancialPlanning.WebAPI/Controllers/PlanController.cs
@@ -1,4 +1,8 @@
+using AutoMapper;
 using FinancialPlanning.Data.Entities;
+using FinancialPlanning.Service.Services;
+using FinancialPlanning.WebAPI.Models.Plan;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
@@ -6,23 +10,53 @@ namespace FinancialPlanning.WebAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PlanController : ControllerBase    
+    public class PlanController(IMapper mapper, PlanService planService) : ControllerBase
     {
-        // POST: api/plan
-        // [HttpPost("import")]
-        // public async ActionResult<string> Import(IFormFile file, String user)
-        // {
-        //     if (file == null)
-        //     {
-        //         return BadRequest("No file uploaded");
-        //     }
+        private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        private readonly PlanService _planService = planService ?? throw new ArgumentNullException(nameof(planService));
 
-        //     try {
-        //         await _fileService.Import(file, user);
-        //     }
-        //     return Ok();
-        // }
-    
+        [HttpGet("Planlist")]
+        [Authorize(Roles = "Accountant, FinancialStaff")]
+        public async Task<IActionResult> GetAllPlans()
+        {
+            var plans = await _planService.GetAllPlans();
+            var planListModels = plans.Select(t => _mapper.Map<PlanListModel>(t)).ToList();
+            return Ok(planListModels);
+        }
+        [HttpGet("{id:guid}")]
+        [Authorize(Roles = "Accountant, FinancialStaff")]
+        public async Task<IActionResult> GetPlanById(Guid id)
+        {
+            var plan = await _planService.GetPlanById(id);
+            return Ok(plan);
+        }
 
+        [HttpPost]
+        [Authorize(Roles = "Accountant")]
+        public async Task<IActionResult> CreatePlan(PlanListModel planModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var plan = _mapper.Map<Plan>(planModel);
+                await _planService.CreatePlan(plan);
+                return Ok(new { message = "Plan created successfully!" });
+            }
+
+            return BadRequest(new { error = "Invalid model state!" });
+        }
+
+
+
+
+
+
+
+        [HttpDelete("{id:guid}")]
+        [Authorize(Roles = "Accountant")]
+        public async Task<IActionResult> DeletePlan(Guid id)
+        {
+            await _planService.DeletePlan(id);
+            return Ok(new { message = $"Plan with id {id} deleted successfully!" });
+        }
     }
 }
