36bc5f73e73660b78dcf7b126e17849147f6acd9
Nguyen Tran Minh (K17 HL) <minhnthe176001@fpt.edu.vn>
2024-03-18 08:52:49 +0700

    Add animation provider

--- FinancialPlaningUI/FinancialPlaning/src/app/app.config.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/app.config.ts
@@ -1,18 +1,19 @@
-import { HttpClient, HttpClientModule, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient} from '@angular/common/http';
 import { authInterceptor } from './services/auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
  
 export const appConfig: ApplicationConfig = {
   providers: [
-    // importProvidersFrom(HttpClientModule),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideHttpClient(withFetch()),
     provideClientHydration(),
-    provideRouter(routes),  
+    provideRouter(routes),
+    provideAnimationsAsync()
   ],
    
 };
