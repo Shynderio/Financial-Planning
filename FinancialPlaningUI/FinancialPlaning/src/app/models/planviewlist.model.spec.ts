90c78bd35d772c77afc87a29b9846da1a209cfc7
DESKTOP-8D3LMBI\hihih <hihihi652652@gmail.com>
2024-03-18 09:34:44 +0700

    Merge branch 'sprint-03' into UC-10-View-list-of-Financial-Plan
    # Conflicts:
    #	FinancialPlanningBE/FinancialPlanning.WebAPI/Controllers/PlanController.cs
    #	FinancialPlanningBE/FinancialPlanning.WebAPI/Helpers/MappingProfile.cs

---
+++ FinancialPlaningUI/FinancialPlaning/src/app/models/planviewlist.model.spec.ts
@@ -0,0 +1,7 @@
+import { Planviewlist } from './planviewlist.model';
+
+describe('Planviewlist', () => {
+  it('should create an instance', () => {
+    expect(new Planviewlist()).toBeTruthy();
+  });
+});
