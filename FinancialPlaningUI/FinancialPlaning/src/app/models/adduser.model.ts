bdfed85e7c20029448877acd483c3d8e22d7bb44
MSI\tuanm <tuantnhe163211@fpt.edu.vn>
2024-03-18 11:59:38 +0700

    userlist

---
+++ FinancialPlaningUI/FinancialPlaning/src/app/models/adduser.model.ts
@@ -0,0 +1,13 @@
+export interface AddUserModel{
+    fullName: string;
+    username: string;
+    email: string;
+    phoneNumber: number;
+    dob: string;
+    address: string;
+    roleId : string,
+    departmentId: string;
+    positionId: string;
+    status: number;
+    notes: string;
+}
