96552eaab47b02f8f496e832ee3015bd9d07f976
Vuong Cong Minh <hihihi652652@gmail.com>
2024-03-21 16:22:31 +0700

    Fix ModelView

--- FinancialPlanningBE/FinancialPlanning.WebAPI/Models/Plan/PlanViewModel.cs
+++ FinancialPlanningBE/FinancialPlanning.WebAPI/Models/Plan/PlanViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class PlanViewModel
     {
-        public int No { get; set; }
+        public Guid Id { get; set; }
         public string Plan { get; set; } = String.Empty; 
         public string Term { get; set; } = String.Empty;
         public string Department { get; set; } = String.Empty;
