9c36b65d2db0763039e2794e8d395c22919dacb7
Trinh Ngoc Tuan <tuantnhe163211@fpt.edu.vn>
2024-03-22 13:44:11 +0700

    update user service

--- FinancialPlaningUI/FinancialPlaning/src/app/services/user.service.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/services/user.service.ts
@@ -7,50 +7,58 @@ import { IDepartment } from '../models/department-list';
 import { IRole } from '../models/role-list';
 import { IPosition } from '../models/position-list';
 import { Observable, catchError, map } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  apiUrl = "http://localhost:5085";
+  private apiUrl = environment.apiUrl + '/User';
 
   constructor(private http: HttpClient) { }
 
   getAllUser() {
-    return this.http.get<UserModel[]>(this.apiUrl + "/api/User")
+    return this.http.get<UserModel[]>(this.apiUrl)
   }
 
   getUserById(userId: string) {
-    return this.http.get<EditUser[]>(this.apiUrl + '/api/User/' + userId)
+    return this.http.get<EditUser[]>(this.apiUrl +'/'+ userId)
   }
 
   editUser(userId: string, user: AddUser) {
-    return this.http.put<AddUser>(this.apiUrl + '/api/User/' + userId, user)
+    return this.http.put<AddUser>(this.apiUrl + '/' + userId, user)
   }
   changeUserStatus(userId: string, status: number): Observable<number> {
-    return this.http.put(`http://localhost:5085/api/User/${userId}/${status}`, {}, { observe: 'response', responseType: 'text' })
-      .pipe(
-        map((response: HttpResponse<any>) => response.status),
-        catchError((error: HttpErrorResponse) => {
-          console.error('Error occurred:', error);
-          throw error;
-        })
-      );
+    return this.http.put(this.apiUrl + '/' + userId + '/' + status,{},{ observe: 'response', responseType: 'text' }).pipe(
+      map((response: HttpResponse<any>) => response.status),
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error occurred:', error);
+        throw error;
+      })
+    );
+    // return this.http.put(`http://localhost:5085/api/User/${userId}/${status}`, {}, { observe: 'response', responseType: 'text' })
+    //   .pipe(
+    //     map((response: HttpResponse<any>) => response.status),
+    //     catchError((error: HttpErrorResponse) => {
+    //       console.error('Error occurred:', error);
+    //       throw error;
+    //     })
+    //   );
   }
 
   addNewUser(user: AddUser) {
-    return this.http.post(this.apiUrl + "/api/User", user);
+    return this.http.post(this.apiUrl, user);
   }
 
   getAllDepartment() {
-    return this.http.get<IDepartment[]>(this.apiUrl + "/api/User/AllDepartments")
+    return this.http.get<IDepartment[]>(this.apiUrl + '/'+'AllDepartments')
   }
 
   getRole() {
-    return this.http.get<IRole[]>(this.apiUrl + "/api/User/AllRoles")
+    return this.http.get<IRole[]>(this.apiUrl + '/'+'AllRoles')
   }
 
   getPosition() {
-    return this.http.get<IPosition[]>(this.apiUrl + "/api/User/AllPositions")
+    return this.http.get<IPosition[]>(this.apiUrl + '/'+'AllPositions')
   }
 }
