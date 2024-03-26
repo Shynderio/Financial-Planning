bb690fadfbebc88f6577a98837510984a3bce34d
Nguyen Tran Minh <minhnt1307@gmail.com>
2024-03-26 11:05:20 +0700

    re-style authentication page, add notification

--- FinancialPlaningUI/FinancialPlaning/src/app/pages/auth/login/login.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/pages/auth/login/login.component.ts
@@ -1,21 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Renderer2 } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LoginModel } from '../../models/loginModel.model';
-import { AuthService } from '../../services/auth/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef } from '@angular/core';
+import { AuthService } from '../../../services/auth/auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MESSAGE_CONSTANTS } from '../../../../constants/message.constants';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  MESSAGE_CONSTANTS = MESSAGE_CONSTANTS;
+
+  isLoading = false;
+
   loggedInUser: any; // Khai báo biến để lưu thông tin người dùng đã đăng nhập
   errorMessage = '';
   loginForm!: FormGroup;
@@ -24,38 +46,30 @@ export class LoginComponent {
 
   constructor(
     private authService: AuthService,
-    private http: HttpClient,
     private router: Router,
     private formBuilder: FormBuilder,
-    private renderer: Renderer2,
-    private el: ElementRef,
-    private cookieService: CookieService
+    private elementRef: ElementRef
   ) {
     // localStorage.clear();
   }
 
   ngOnInit(): void {
     // check Islogged
-    if(this.authService.IsLoggedIn()){
-      console.log(this.authService.IsLoggedIn()); 
-      this.router.navigate(['/home']);       
+    if (this.authService.IsLoggedIn()) {
+      console.log(this.authService.IsLoggedIn());
+      this.router.navigate(['/home']);
     }
 
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
-
-    this.renderer.addClass(this.el.nativeElement, 'backgroundLogin');
   }
 
   //Login
   login(): void {
-    if (this.loginForm.invalid) {
-      this.errorMessage = '';
-      this.loginClicked = true;
-      return;
-    }
+    if (this.loginForm.invalid) return;
+    this.isLoading = true;
     this.authService.login(this.loginForm.value).subscribe({
       next: (response: any) => {
         console.log(response); // Log response to the console
@@ -64,20 +78,21 @@ export class LoginComponent {
         if (response.statusCode == 200) {
           //Save token
           const token = response?.value?.token;
-          localStorage.setItem('token',token);
-         
+          localStorage.setItem('token', token);
+
           //Go to home page
           this.router.navigateByUrl('/home').then(() => {
             window.location.reload();
           });
         } else {
-          this.loginClicked = false;
-          this.errorMessage =
-            'Either email address or password is incorrect. Please try again';
-          console.log(this.errorMessage);
+          this.errorMessage = MESSAGE_CONSTANTS.ME001;
+          this.isLoading =
+            false;
         }
       },
       error: (error) => {
+        this.errorMessage = MESSAGE_CONSTANTS.ME000;
+        this.isLoading = false;
         console.error(error); // Log error to the console
       },
     });
