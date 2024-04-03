3ccf8a35fa9edcf2d041dfbe9685fbe336682b1e
Nguyen Duc Thinh <nguyenducthinhch2@gmail.com>
2024-04-03 09:18:59 +0700

    add state to file form

--- FinancialPlaningUI/FinancialPlaning/src/app/share/upload/upload.component.ts
+++ FinancialPlaningUI/FinancialPlaning/src/app/share/upload/upload.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpEventType } from '@angular/common/http';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -19,17 +19,13 @@ export class UploadComponent implements OnInit {
   @Input() isTermSelected: boolean = true; // Variable to store validName
   @Input() isMonthSelected: boolean = true; // Variable to store validName
   @Input() validName: string = ''; // Variable to store validName
+  @Input() state : number = 1;
   file: File | null = null; // Variable to store file
   progress?: number ; // Variable to store progress
 
   constructor(private http: HttpClient, private messagebar: MatSnackBar) { }
 
   ngOnInit() {
-  }
-
-  reset(){
-    this.file = null;
-    
   }
 
   onClick() 
@@ -69,5 +65,13 @@ export class UploadComponent implements OnInit {
   loadForm(file: File, fileName: string) {
     const formData = new FormData();
     formData.append('file', file, fileName);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if (changes['state'].currentValue != changes['state'].previousValue) {
+      this.file = null;
+    }
   }
 }
